1a47ba9b8771d812445dc271c452ac4ec8790780
Hrithvik Alex <halex@seatgeek.com>
2021-06-02 17:16:25 -0400

    updated ltype checking logic to map generic types to other types

--- druzhba/postgres.py
+++ druzhba/postgres.py
@@ -292,13 +292,13 @@ class PostgreSQLTableConfig(TableConfig):
         return size_str
 
     def _format_redshift_type(self, type_name, size_str, name):
-        if name in self.type_map:
-            return self.type_map[name]
-        else:
-            return "{type}{size}".format(
-                    type=type_name, 
-                    size=size_str
-                )
+        final_type = "{type}{size}".format(
+                type=type_name, 
+                size=size_str
+            )
+        if final_type in self.type_map:
+            final_type = self.type_map[final_type]
+        return final_type
 
     def _format_column_query(self, column_name, data_type):
         # PostgreSQL's MONEY type is a bit strange. It's an 8-byte fixed fractional precision value
